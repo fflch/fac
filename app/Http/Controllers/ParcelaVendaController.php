13514c1e0bd4fbdfcc2211d22758564d0fca19c7
Saotome <saotome@usp.br>
2021-06-22 15:58:41 -0300

    Parcialmente fix #25

--- app/Http/Controllers/ParcelaVendaController.php
+++ app/Http/Controllers/ParcelaVendaController.php
@@ -11,9 +11,17 @@ class ParcelaVendaController extends Controller
     public function update(ParcelaVenda $parcelaVenda)
     {
         $this->authorize('admin');
+        $venda = $parcelaVenda->venda;
+        $parcelas = $venda->parcelas;
+        foreach ($parcelas as $parcela) {
+          if ($parcela->status != "Baixado" && $parcela->id < $parcelaVenda->id) {
+            request()->session()->flash('alert-danger', 'Não pode ser baixado, pois a parcela anterior não foi baixada.');
+            return redirect("/vendas/$parcelaVenda->venda_id");
+          }
+        }
         $parcelaVenda->status = 'Baixado';
         $parcelaVenda->update();
-
+        request()->session()->flash('alert-success', 'Parcela baixada com sucesso.');
         return redirect("/vendas/$parcelaVenda->venda_id");
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,9 +3,14 @@
 @section('content')
 
     @can('conveniado')
-        @if ($conveniado) 
+        @if ($conveniado)
+            <form method="get" action="/vendas">
+              @include('vendas.partials.search')
+            </form>
+            <br>
             @include('conveniados.vendas')
+            {{ $vendas->appends(request()->query())->links() }}
         @endif
     @endcan
-    
+
 @endsection
