17025130e865a979f92646f36c88de7ee737ff6f
Flavia Lima <flavialima2002@usp.br>
2022-09-15 16:16:55 -0300

    att

--- resources/views/associados/form.blade.php
+++ resources/views/associados/form.blade.php
@@ -191,22 +191,6 @@
 
 <hr>
 
-<div class="card">
-    <div class="card-header"><b> Nova senha </b></div>
-    <div class="card-body">
-        <div class="col-sm form-group">
-            <div class="form-group">
-                <label for="password" class="required"><b>Senha: </b></label>
-                <br>
-                <small>Deixe este campo em branco para não mudar a senha.</small>
-                <input type="password" class="form-control" id="password" name="password">
-            </div>
-        </div>
-    </div>
-</div>
-
-<hr>
-
 <div class="form-group">
     <button type="submit" class="btn btn-success">Enviar</button>
 </div>
